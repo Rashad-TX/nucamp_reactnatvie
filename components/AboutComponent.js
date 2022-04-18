a21595a097bb9d5bb17c45fb4101173307f7c512
Rashad Moore <rjm4god7@gmail.com>
2022-04-18 15:27:37 -0500

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { FlatList } from "react-native";
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -51,28 +52,32 @@ return(
 
     if (this.props.partners.errMess) {
         return (
-<ScrollView>
+            <ScrollView>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
                     <Mission />
                     <Card
-                        title='Community Partners'>
+                        title="Community Partners">
                         <Text>{this.props.partners.errMess}</Text>
                     </Card>
-                </ScrollView>
-            );
-        }
-
-    return <ScrollView>
-
-<Mission/>
-<Card
-title="Community Partners">
-    <FlatList
-    data={this.props.partners.partners}
-    keyExtractor = {item => item.id.toString()}
-    renderItem={renderPartner}
-    />
-</Card>
-    </ScrollView>
+                </Animatable.View>
+            </ScrollView>
+        );
+    }
+    return (
+        <ScrollView>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Mission />
+                <Card
+                    title="Community Partners">
+                    <FlatList
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item=>item.id.toString()}
+                    />
+                </Card>
+            </Animatable.View>
+        </ScrollView>
+    );
 }
 
 }
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,8 @@ import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { postFavorite } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
+
 
 const mapStateToProps = state => {
     return {
@@ -23,6 +25,7 @@ function RenderCampsite(props) {
 
     if (campsite) {
         return (
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Card 
                 featuredTitle={campsite.name}
                 image={{uri: baseUrl + campsite.image}}>
@@ -47,6 +50,7 @@ function RenderCampsite(props) {
                         onPress={() => props.onShowModal()}
                     />
             </Card>
+              </Animatable.View>
         );
     }
     return <View />;
@@ -65,6 +69,7 @@ function RenderComments({comments}) {
     };
 
     return (
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
         <Card title="Comments">
             <FlatList
                 data={comments}
@@ -72,6 +77,7 @@ function RenderComments({comments}) {
                 keyExtractor={item => item.id.toString()}
             />
         </Card>
+        </Animatable.View>
     );
 }
 
