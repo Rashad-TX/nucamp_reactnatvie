5376eb86cc8f56ba96f5667af3aa0b77a4df607c
Rashad Moore <rjm4god7@gmail.com>
2022-04-25 15:18:59 -0500

    local notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
-
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -35,11 +35,13 @@ class Reservation extends Component {
                     text: 'Cancel',
                     style: 'cancel',
                     onPress: () => this.resetForm()
+                    
                 },
                 {
                     text: 'OK', 
                     onPress: () => {
-                    this.resetForm();
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
                     }
                 }
             ],
@@ -53,6 +55,32 @@ class Reservation extends Component {
             date: ''
         });
     }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
 
 
     resetForm() {
